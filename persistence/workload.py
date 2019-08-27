306393d7114d8fea1a3827e8d59cb1730d152374
denezuela <zagumionnov.denis@yandex.ru>
2019-08-27 23:55:38 +0300

    add persistence layer for workload

---
+++ persistence/workload.py
@@ -0,0 +1,42 @@
+import os
+from migration.workload import Workload
+from migration.json_decoder import get_workload
+from .jsonpickler import JsonPickler
+
+SCRIPT_PATH = os.path.dirname(__file__)
+WORKLOADS_DIRNAME = os.path.join(SCRIPT_PATH, "workloads")
+
+
+class WorkloadPickler(JsonPickler):
+    def __init__(self, dirname=WORKLOADS_DIRNAME):
+        super().__init__(dirname=dirname)
+
+    def get_id(self, workload: Workload) -> str:
+        return workload.ip
+
+    def get_serializable(self, s: str) -> Workload:
+        return get_workload(s)
+
+    def is_possible_to_create(self, workload: Workload) -> (bool, str):
+        for filename in os.listdir(self.dirname):
+            with open(os.path.join(self.dirname, filename), "r") as f:
+                current = self.get_serializable(f.read())
+
+                if current.ip == workload.ip:
+                    return False, "Cannot create record with the same IP value"
+
+        return True, ""
+
+    def is_possible_to_update(self, old_workload: object, new_workload: object) -> (bool, str):
+        if old_workload.ip != new_workload.ip:
+            return False, "Cannot update IP value"
+
+        return True, ""
+
+    def fits(self, pickle_object: Workload, object: Workload) -> bool:
+        if pickle_object is not None and object is not None:
+            return pickle_object.ip == object.ip
+        else:
+            return pickle_object is None and object is None
+
+        return True
---
+++ tests/test_workload_persistence.py
@@ -0,0 +1,73 @@
+import unittest
+import os
+
+from migration.workload import Workload
+from migration.credentials import Credentials
+from migration.mount_point import MountPoint
+
+from persistence.workload import WorkloadPickler
+
+SCRIPT_PATH = os.path.dirname(__file__)
+WORKLOADS_DIRNAME = os.path.join(SCRIPT_PATH, "workloads")
+
+workload_pickler = WorkloadPickler(dirname=WORKLOADS_DIRNAME)
+
+
+class MyTestCase(unittest.TestCase):
+    def setUp(self):
+        for filename in os.listdir(WORKLOADS_DIRNAME):
+            os.remove(os.path.join(WORKLOADS_DIRNAME, filename))
+
+    def test_create_and_read_credentials(self):
+        credentials = Credentials("username", "password", "domain")
+        workload = Workload("192.168.0.1", credentials, [MountPoint("C:\\", 1024)])
+        workload_pickler.create(workload)
+        ret = workload_pickler.read(workload)
+        self.assertEqual(len(ret), 1)
+        self.assertEqual(ret[0].ip, "192.168.0.1")
+        self.assertEqual(ret[0].credentials.username, "username")
+        self.assertEqual(ret[0].storage[0].mount_point_name, "C:\\")
+
+    def test_update(self):
+        credentials = Credentials("user", "qwerty", None)
+        workload = Workload("8.8.8.8", credentials, [MountPoint("C:\\", 1024)])
+        workload_pickler.create(workload)
+
+        credentials = Credentials("new_user", "qwerty", None)
+        new_workload = Workload("8.8.8.8", credentials, [MountPoint("C:\\", 1024)])
+        workload_pickler.update(workload, new_workload)
+
+        ret = workload_pickler.read(new_workload)
+        self.assertEqual(len(ret), 1)
+        self.assertEqual(ret[0].ip, "8.8.8.8")
+        self.assertEqual(ret[0].credentials.username, "new_user")
+        self.assertEqual(ret[0].storage[0].mount_point_name, "C:\\")
+
+    def test_create_with_the_same_ip(self):
+        credentials = Credentials("username", "password", "domain")
+        workload = Workload("1.1.1.1", credentials, [MountPoint("C:\\", 1024)])
+
+        workload_pickler.create(workload)
+        with self.assertRaises(Exception):
+            workload = Workload("1.1.1.1", credentials, [MountPoint("E:\\", 1024)])
+            workload_pickler.create(workload)
+
+    def test_update_with_the_same_ip(self):
+        credentials = Credentials("username", "password", "domain")
+        workload = Workload("4.4.4.4", credentials, [MountPoint("C:\\", 1024)])
+
+        workload_pickler.create(workload)
+        with self.assertRaises(Exception):
+            workload = Workload("4.4.4.4", credentials, [MountPoint("E:\\", 1024)])
+            workload_pickler.update(workload)
+
+    def test_delete(self):
+        credentials = Credentials("temp_user", "password", "domain")
+        workload = Workload("3.3.3.3", credentials, [MountPoint("C:\\", 1024)])
+        workload_pickler.create(workload)
+        workload_pickler.delete(workload)
+        self.assertEqual(workload_pickler.read(workload), [])
+
+
+if __name__ == '__main__':
+    unittest.main()
